bfb14428baeee25ab5747e23f320761073e09dde
“Lanyi-Fur” <Lanyi_adict@outlook.com>
2024-11-14 23:04:32 +0800

    Update the report

--- Java Programming for SXIT/Final_Project/src/LoginAPP.java
+++ Java Programming for SXIT/Final_Project/src/LoginAPP.java
@@ -1,8 +1,27 @@
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginAPP extends JFrame{
     private JTextField usernameField;
     private JPasswordField passwordField;
+    public LoginAPP(){
+        setTitle("欢迎来到扫雷");
+        setSize(500,250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+
+        JLabel usernamLabel = new JLabel("Username:");
+        add(usernamLabel);
+
+        usernameField = new JTextField();
+        add(usernameField);
+        
+    }
 } 
