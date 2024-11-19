d0ac8cb506f9ae469dafcb0e930c2e07eae586fa
“Lanyi-Fur” <Lanyi_adict@outlook.com>
2024-11-19 16:50:43 +0800

    Refresh the Assignment4

--- MIT6.092/Assignment4/Book.java
+++ MIT6.092/Assignment4/Book.java
@@ -1,4 +1,3 @@
-package Library;
 public class Book {
 
     String title;
@@ -8,7 +7,6 @@ public class Book {
     public Book(String bookTitle) {
         // Implement this method
         title = bookTitle;
-        borrowed = false;
     }
    
     // Marks the book as rented
@@ -36,13 +34,13 @@ public class Book {
     }
 
     public static void main(String[] arguments) {
-		// Small test of the Book class
-		Book example = new Book("The Da Vinci Code");
-		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-		example.borrowed();
-		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-		example.returned();
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-	}
+        // Small test of the Book class
+        Book example = new Book("The Da Vinci Code");
+        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        example.returned();
+        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+        example.returned();
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+    }
 } 
